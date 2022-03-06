cb67bd83f51404966e6a6e94a20175557f7068f7
n_bonnie2 <96018845+nbonnie2@users.noreply.github.com>
2022-03-06 15:40:24 -0500

    Delete Vs EpicBoiz directory

--- Vs EpicBoiz/custom_events/Screen Flash.lua
+++
@@ -1,8 +0,0 @@
--- Event notes hooks
-function onEvent(name, value1, value2)
-	if name == 'Screen Flash' then
-		if 'flashingLights' then
-		cameraFlash(game, value2, value1, false)
-		end
-	end
-end
